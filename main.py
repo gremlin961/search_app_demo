d3eb0b57a58c87a3911c6d6b8cc7985ec3e8aafc
Richard Kiles <rkiles@google.com>
2024-04-23 18:22:53 -0400

    Updated UI to render markdown and move results to the right of the form.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from pkg import vertexModels
 from fastapi.staticfiles import StaticFiles
 import yaml
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import markdown
 
 
 app = FastAPI()
@@ -74,48 +75,38 @@ async def load_search_results(request: Request):
   #grounding_data = grounding_data.replace("\n", "<br />")
   return grounding_data
 
-
-
-@app.post("/generate")
-async def generate(request: Request, hunting_ground: str = Form(...), grounding: str = Form(...), persona: str = Form(...), objective: str = Form(...), context: str = Form(...), output_format: str = Form(...)):
+@app.post("/load_gemini_response")
+async def load_gemini_response(request: Request):
+    print("Generating Gemini Response...")
+    data = await request.json()
+    if (data["hunting_ground"] == 'Custom'):
+        search_query = data["custom_query"]
+    else: 
+        search_query = "Tell me about the " + data["hunting_ground"] + " HG"
     
-    # Initialize the variables
-    #query = f"""Tell me about the {hunting_ground} HG"""
+    prompt = f"""<Hunting Ground> {data["hunting_ground"]}
 
-    
-    #print(context)
-    
-    # Perform the search query
-    #print('performing search query now')
-    #search_results = vertexModels.search_sample(project_id, location, engine_id, query)
+<Persona>
+{data["persona"]}
 
-    #search_summary = search_results.summary.summary_text
-    
-    print(grounding)
+<Objective>
+{data["objective"]}
 
-    # Construct the prompt
-    prompt = f"""**Hunting Ground:** {hunting_ground}
+<context>
+{data["context"]}
 
-**Persona:** {persona}
+<Grounding Data>
+{data["grounding"]}
 
-**Objective:** {objective}
+<Output Format>
+{data["output_format"]}"""
 
-**Context:**
-{context}
-
-**Grounding Data:**
-{grounding}
-
-**Output Format:** {output_format}"""
-
-    # Call the Gemini model
-    response = vertexModels.gemini_text(project_id, region, prompt)
-
-    # Post-process the response to add new lines
-    response = response.replace("\n", "<br>")
-
-    # Render the results page
-    return templates.TemplateResponse("index.html", {"request": request, "grounding":grounding, "persona": persona, "objective": objective, "context": context, "output_format": output_format, "hunting_ground": hunting_ground, "response": response})
+    print(prompt)
+    gemini_results = vertexModels.gemini_text(project_id, region, prompt)
+    #gemini_results = gemini_results.replace("\n", "<br />")
+    gemini_results = markdown.markdown(gemini_results)
+    print(gemini_results)
+    return gemini_results
 
 
 if __name__ == "__main__":
--- static/scripts.js
+++ static/scripts.js
@@ -1,39 +1,78 @@
 document.getElementById("load-button").addEventListener("click", function() {
-    // Show the loading screen
-    document.getElementById("loading-screen").style.display = "block";
+  // Show the loading screen
+  document.getElementById("loading-screen").style.display = "block";
 
-    // Make AJAX call to load search results
-    fetch("/load_search_results", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        hunting_ground: document.getElementById("hunting_ground").value,
-        custom_query: document.getElementById("custom_query").value
-      })
+  // Make AJAX call to load search results
+  fetch("/load_search_results", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      hunting_ground: document.getElementById("hunting_ground").value,
+      custom_query: document.getElementById("custom_query").value
     })
-    .then(response => response.text())
-    .then(data => {
-        // Hide the loading screen
-        document.getElementById("loading-screen").style.display = "none";
-        // Replace \n characters with actual line breaks
-        data = data.replace(/\\n/g, "\n");
-        // Update the search-results textarea with the response
-        document.getElementById("grounding").value = data;
-    });
+  })
+  .then(response => response.text())
+  .then(data => {
+      // Hide the loading screen
+      document.getElementById("loading-screen").style.display = "none";
+      // Replace \n characters with actual line breaks
+      data = data.replace(/\\n/g, "\n");
+      // Update the search-results textarea with the response
+      document.getElementById("grounding").value = data;
   });
+});
+
+
+
+
+document.getElementById("generate").addEventListener("click", function() {
+// Show the loading screen
+document.getElementById("loading-screen").style.display = "block";
+
+// Make AJAX call to load search results
+fetch("/load_gemini_response", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    hunting_ground: document.getElementById("hunting_ground").value,
+    persona: document.getElementById("persona").value,
+    objective: document.getElementById("objective").value,
+    context: document.getElementById("context").value,
+    grounding: document.getElementById("grounding").value,
+    output_format: document.getElementById("output_format").value
+  })
+})
+.then(response => response.text())
+.then(data => {
+    // Hide the loading screen
+    document.getElementById("loading-screen").style.display = "none";
+    // Replace \n characters with actual line breaks
+    data = data.replace(/\\n/g, "\n");
+    // Update the search-results textarea with the response
+    //document.getElementById("generated_result").value = data;
+    document.getElementById("generated_result").innerHTML = data;
+    // Show the form with results
+    document.getElementById("results_form").style.display = "block";
+});
+});
+
+
+
 
 // Add event listener for the dropdown selection change
 document.getElementById("hunting_ground").addEventListener("change", function() {
-  // Check if "custom" is selected
-  if (this.value === "Custom") {
-    // Show the custom query textarea and label
-    document.getElementById("custom_query_label").style.display = "block";
-    document.getElementById("custom_query").style.display = "block";
-  } else {
-    // Hide the custom query textarea and label
-    document.getElementById("custom_query_label").style.display = "none";
-    document.getElementById("custom_query").style.display = "none";
-  }
+// Check if "custom" is selected
+if (this.value === "Custom") {
+  // Show the custom query textarea and label
+  document.getElementById("custom_query_label").style.display = "block";
+  document.getElementById("custom_query").style.display = "block";
+} else {
+  // Hide the custom query textarea and label
+  document.getElementById("custom_query_label").style.display = "none";
+  document.getElementById("custom_query").style.display = "none";
+}
 });
